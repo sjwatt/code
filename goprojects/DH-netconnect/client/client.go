0f950e2fb2555cb7840b3d766395dc5e7a6635c9
simon <simon@UAlpha.(none)>
2010-08-07 14:07:12 -0700

    lots of work on the crypto project, as well as adding the ddclient script

--- goprojects/DH-netconnect/client/client.go
+++ goprojects/DH-netconnect/client/client.go
@@ -7,6 +7,7 @@ import (
 	"time"		// Package to support time.Sleep command
 	"strings"
 	"bufio"
+	"rand"
 )
 
 // Struct type for Data communication
@@ -16,7 +17,7 @@ type value struct {
 }
 
 var begin int64
-
+var sharedSecret int
 func lt() {fmt.Print("[ ",time.Nanoseconds() / 1e6 - begin,"]: ")}
 
 // Factory function to create communication channel with server
@@ -31,7 +32,7 @@ func inChanFactory() (chan value, os.Error){
 	//Make the incoming communication channel for this program
 	ch := make(chan value)
 	//Link the ch channel to the import channel
-	err = imp.Import("exportChannelSend",ch,netchan.Recv,new(value))
+	err = imp.Import("exportChannelSend",ch,netchan.Recv)
 	if err != nil {
 		return nil, err
 	}
@@ -48,16 +49,18 @@ func outChanFactory() (chan value, os.Error){
 	//Make the outgoing communication channel for this program
 	ch := make(chan value)
 	//Link the ch channel to the import channel
-	err = imp.Import("exportChannelRecv",ch,netchan.Send,new(value))
+	err = imp.Import("exportChannelRecv",ch,netchan.Send)
 	if err != nil {
 		return nil, err
 	}
 	return ch, err	
 }
+
 func printIncoming(inChan chan value, done chan bool) {
 	for ; !closed(inChan) ; {
 		inval := <- inChan
 		lt();fmt.Println("Data recieved from server: ",inval)
+		fmt.Println(decrypt(inval.s))
 	}
 	done <- true
 }
@@ -67,39 +70,125 @@ func acceptOutgoing(outChan chan value, done chan bool) {
 	for i:= 0 ; !closed(outChan); i ++ {
 		result, _ := input.ReadString('\n')
 		text := strings.TrimSpace(result)
-		outval := value{i,text}
+		outval := value{i,encrypt(text)}
 		lt();fmt.Println("Sending data to outChan: ",outval)
 		outChan <- outval
 		lt();fmt.Println("Data sent")
 	}
 	done <- true
 }
+
+func keyExchangeClient(outChan chan value, inChan chan value) (success bool) {
+
+	goodConnection := false
+	//receive Prime p from server
+	inPrime := <- inChan
+	lt();fmt.Println(inPrime)
+	//receive base g from server
+	inBase := <- inChan
+	lt();fmt.Println(inBase)
+	//receive Server's public key from server
+	inServerPubKey := <- inChan
+	lt();fmt.Println(inServerPubKey)
+	
+	//Generate secret integer b
+	randMax := 100000000
+	rand.Seed(time.Nanoseconds())
+	secretInt := rand.Int() % randMax
+	lt();fmt.Println("Secret Integer:",secretInt)
+
+	//Generate Client Public key
+	//g^b mod p
+	PubKey := (inBase.i^secretInt)%(inPrime.i)
+	lt();fmt.Println("Client Public Key:",PubKey)
+	
+	//Send Client PubKey to server
+	outChan <- value{PubKey,"Client Public Key"}
+	
+	//Calculate Shared Secret
+	//ClientPubKey^SecretInt mod prime
+	sharedSecret = (inServerPubKey.i ^ secretInt) % inPrime.i
+	lt();fmt.Println("Calculated Shared Secret:",sharedSecret)
+	
+	
+	
+	//Answer Server's Challenge
+	//response = challenge^sharedSecret mod prime
+	inChallenge := <- inChan
+	lt();fmt.Println(inChallenge)
+	response := (inChallenge.i ^ sharedSecret) % inPrime.i
+	lt();fmt.Println("Response to Server:",response)
+	outChan <- value{response,"Response From Client"}
+	
+	//Send encrypted challenge to Server
+	//challenge formula: nonce^sharedSecret mod prime
+	//Server: challenge^sharedSecret mod prime
+	//Client checks result
+	challenge := ((rand.Int()%randMax)^sharedSecret) % inPrime.i
+	lt();fmt.Println("Challenge to Server:",challenge)
+	expectedResult := (challenge^sharedSecret) % inPrime.i
+	lt();fmt.Println("Expected Result:",expectedResult)
+	outChan <- value{challenge,"Challenge From Client"}
+	result := <- inChan
+	lt();fmt.Println(result)
+	if result.i == expectedResult {
+		goodConnection = true
+	}
+	return goodConnection
+}
+
+//simple encryption routine
+func encrypt(input string) (result string){
+	data := []int(input)
+	for i := 0; i < len(data); i ++ {
+		data[i] += sharedSecret % 26
+	}
+	result = string(data)
+	return result
+}
+
+//simple decryption routine
+func decrypt(input string) (result string){
+	data := []int(input)
+	for i := 0; i < len(data); i ++ {
+		data[i] -= sharedSecret % 26
+	}
+	result = string(data)
+	return result
+}
 func comRoutine() {
 	//Use the factory to create a new network communication channel, in and out
 	inChan, inerr := inChanFactory()
-	outChan, outerr := outChanFactory()
+	
 	if inerr != nil {
 		lt();fmt.Println("inChanFactory error: ",inerr)
 		return
 	}else{
 		lt();fmt.Println("Returned to main after inChan made")
 	}
+	outChan, outerr := outChanFactory()
 	if outerr != nil {
 		lt();fmt.Println("outChanFactory error: ",outerr)
 		return
 	}else{
 		lt();fmt.Println("Returned to main after outChan made")
 	}
-	
-	inDone := make(chan bool)
-	outDone := make(chan bool)
-	go printIncoming(inChan,inDone)
-	go acceptOutgoing(outChan,outDone)
-	select {
-		case <- inDone:lt();fmt.Println("Incoming Channel quit")
-		case <- outDone:lt();fmt.Println("Outgoing Channel quit")
+
+	if keyExchangeClient(outChan, inChan) {
+		lt();fmt.Println("Keyexchange Success")
+		inDone := make(chan bool)
+		outDone := make(chan bool)
+		go printIncoming(inChan,inDone)
+		go acceptOutgoing(outChan,outDone)
+		select {
+			case <- inDone:lt();fmt.Println("Incoming Channel quit")
+			case <- outDone:lt();fmt.Println("Outgoing Channel quit")
+		}
+		lt();fmt.Println("Killing other thread")
+	} else {
+		lt();fmt.Println("Keyexchange Failed")
 	}
-	lt();fmt.Println("Killing other thread")	
+	time.Sleep(1e9)
 }
 func main() {
 	//initialize program time
@@ -108,6 +197,6 @@ func main() {
 		// Attempt to form a com channel, and run if siccessful
 		comRoutine()
 		// If com channel quits/fails then wait 1 second and run again, forever.
-		time.Sleep(1e9)
+		time.Sleep(2e9)
 	}
 }
--- goprojects/DH-netconnect/server/server.go
+++ goprojects/DH-netconnect/server/server.go
@@ -7,6 +7,7 @@ import (
 	"bufio"
 	"strings"
 	"time"
+	"rand"
 )
 
 // Struct type for Data communication
@@ -16,7 +17,8 @@ type value struct {
 }
 
 var begin int64
-
+var sharedSecret int
+//Macro helper to print program time
 func lt() {fmt.Print("[ ",time.Nanoseconds() / 1e6 - begin,"]: ")}
 
 // Factory function to create listening communication channel that will wait for a connection from the client
@@ -31,7 +33,7 @@ func outChanFactory() (chan value, os.Error){
 	//Make the communication channel for this program
 	ch := make(chan value)
 	//Link the ch channel to the export channel
-	err = exp.Export("exportChannelSend",ch,netchan.Send,new(value))
+	err = exp.Export("exportChannelSend",ch,netchan.Send)
 	if err != nil {
 		return nil, err
 	}
@@ -49,7 +51,7 @@ func inChanFactory() (chan value, os.Error){
 	//Make the communication channel for this program
 	ch := make(chan value)
 	//Link the ch channel to the export channel
-	err = exp.Export("exportChannelRecv",ch,netchan.Recv,new(value))
+	err = exp.Export("exportChannelRecv",ch,netchan.Recv)
 	if err != nil {
 		return nil, err
 	}
@@ -58,7 +60,8 @@ func inChanFactory() (chan value, os.Error){
 func printIncoming(inChan chan value, quit chan bool) {
 	for ;  !closed(inChan) ; {
 		inval := <- inChan
-		lt();fmt.Println("Data recieved from server: ",inval)
+		lt();fmt.Println("Data recieved from Client: ",inval)
+		fmt.Println(decrypt(inval.s))
 	}
 	quit <- true
 }
@@ -67,40 +70,162 @@ func acceptOutgoing(outChan chan value, quit chan bool) {
 	input := bufio.NewReader(os.Stdin)
 	for i:= 0 ; !closed(outChan); i ++ {
 		result, _ := input.ReadString('\n')
-		if closed(outChan) { quit <- true }
+		
 		text := strings.TrimSpace(result)  
-		outval := value{i,text}
+		outval := value{i,encrypt(text)}
 		lt();fmt.Println("Sending data to outChan: ",outval)
 		outChan <- outval
 		lt();fmt.Println("Data sent")
 	}
 	quit <- true
 }
-func main() {
-	//initialize program time
-	begin = time.Nanoseconds() / 1e6
+
+func keyExchangeServer(outChan chan value, inChan chan value) (success bool) {
+
+	goodConnection := false
+	//Generate random prime p (very dumb algorithm)
+	lt();fmt.Println("Generating Prime Number p")
+	rand.Seed(time.Nanoseconds())
+	randMax := rand.Int()
+	randMax = randMax % 100000000
+	//computedPrime := 0
+	running := true
+	prime := randMax
+	for ; (prime > 0); prime -- {
+		running = false
+		for j := 2; j < randMax / 2; j ++ {
+			if prime % j == 0 {
+				running = true
+				break
+			}
+		}
+		if running == false {
+			break
+		}
+	}
+	lt();fmt.Println("Prime p found:",prime)
+	
+	//generate base g which is coprime to p
+	//every number except 1 is coprime to p since p is prime
+	base := randMax - (rand.Int() % randMax)
+	lt();fmt.Println("Base g:",base)
+	
+	//Send p and g to the client
+	outChan <- value{prime,"prime"}
+	outChan <- value{base,"base"}
+	
+	//Choose secret integer a
+	secretInt := rand.Int() % randMax
+	lt();fmt.Println("Secret Integer:",secretInt)
+
+	//Calculate G^a mod p (Public Key)
+	PubKey := (base^secretInt)%(prime)
+	lt();fmt.Println("Server Public Key:",PubKey)
+	
+	//Send Public Key to Client
+	outChan <- value{PubKey,"Server Public Key"}
+	
+	//receive Client PubKey
+	inClientPubKey := <- inChan
+	lt();fmt.Println(inClientPubKey)
+	
+	//Calculate Shared Secret
+	//ClientPubKey^SecretInt mod prime
+	sharedSecret = (inClientPubKey.i ^ secretInt) % prime
+	lt();fmt.Println("Calculated Shared Secret:",sharedSecret)
+	
+	//Send encrypted challenge to client
+	//challenge formula: nonce^sharedSecret mod prime
+	//client: challenge^sharedSecret mod prime
+	//server checks result
+	challenge := ((rand.Int()%randMax)^sharedSecret) % prime
+	lt();fmt.Println("Challenge to Client:",challenge)
+	expectedResult := (challenge^sharedSecret) % prime
+	lt();fmt.Println("Expected Result:",expectedResult)
+	outChan <- value{challenge,"Challenge From Server"}
+	result := <- inChan
+	lt();fmt.Println(result)
+	if result.i == expectedResult {
+		goodConnection = true
+	}
+	
+	//Answer Client's Challenge
+	//response = challenge^sharedSecret mod prime
+	inChallenge := <- inChan
+	lt();fmt.Println(inChallenge)
+	response := (inChallenge.i ^ sharedSecret) % prime
+	lt();fmt.Println("Response to Client:",response)
+	outChan <- value{response,"Response From Server"}
+	
+	
+	
+	
+	
+	return goodConnection
+}
+func comRoutine() {
+	
 	//Create a new [value] variable
 	//Use the factory to create a new network communication channel
-	outChan, outerr := outChanFactory()
-	if outerr != nil {
-		lt();fmt.Println("outChanFactory error: ",outerr)
-	}
-	lt();fmt.Println("Returned to main after outChan made")
 	inChan, inerr := inChanFactory()
 	if inerr != nil {
 		lt();fmt.Println("inChanFactory error: ",inerr)
+		return
+	}else{
+		lt();fmt.Println("Returned to main after inChan made")
 	}
-	lt();fmt.Println("Returned to main after inChan made")
+	outChan, outerr := outChanFactory()
+	if outerr != nil {
+		lt();fmt.Println("outChanFactory error: ",outerr)
+		return
+	}else{
+		lt();fmt.Println("Returned to main after outChan made")
+	}
 	
-	//Write all data recieved from stardard input to the network channel
-	inQuit := make(chan bool)
-	outQuit := make(chan bool)
-	go printIncoming(inChan,inQuit)
-	go acceptOutgoing(outChan, outQuit)
-	switch {
-		case <- inQuit:lt();fmt.Println("Incoming Channel quit")
-		case <- outQuit:lt();fmt.Println("Outgoing Channel quit")
+	if keyExchangeServer(outChan,inChan) {
+		lt();fmt.Println("Keyexchange Success")
+		inQuit := make(chan bool)
+		outQuit := make(chan bool)
+		go printIncoming(inChan,inQuit)
+		go acceptOutgoing(outChan,outQuit)
+		select {
+			case <- inQuit:lt();fmt.Println("Incoming Channel quit")
+			case <- outQuit:lt();fmt.Println("Outgoing Channel quit")
+		}
+		lt();fmt.Println("Killing other thread")
+	} else {
+		lt();fmt.Println("Key Exchange Failed")
+		close(outChan)
+		close(inChan)
 	}
-	os.Exit(1)
+}
+
+//simple encryption routine
+func encrypt(input string) (result string){
+	data := []int(input)
+	for i := 0; i < len(data); i ++ {
+		data[i] += sharedSecret % 26
+	}
+	result = string(data)
+	return result
+}
+
+//simple decryption routine
+func decrypt(input string) (result string){
+	data := []int(input)
+	for i := 0; i < len(data); i ++ {
+		data[i] -= sharedSecret % 26
+	}
+	result = string(data)
+	return result
+}
+func main() {
+	//initialize program time
+	begin = time.Nanoseconds() / 1e6
+	
+		// Attempt to form a com channel, and run if successful
+		comRoutine()
+		// If com channel quits/fails then wait 1 second and run again, forever.
+		
 	
 }
